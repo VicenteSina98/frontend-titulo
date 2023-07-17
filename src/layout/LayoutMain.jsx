014ad5bc85c4d2146cad99a265042121aaf844c0
VicenteSina98 <v.siaalamo1@uandresbello.edu>
2023-07-17 08:19:19 -0400

    🚧 historial en desarrollo

---
+++ src/layout/LayoutMain.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+const LayoutMain = () => (
+  <div className="flex h-screen w-screen bg-slate-50 dark:bg-neutral-700">
+    <Sidebar />
+    <Outlet />
+  </div>
+);
+
+export default LayoutMain;
---
+++ src/pages/History.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const History = () => {
+  useEffect(() => {}, []);
+  return (
+    <main className="mx-auto mb-8 mt-20 flex w-2/3 flex-col justify-start gap-8">
+      <h2 className="text-left text-2xl font-bold dark:text-white">
+        Mis predicciones
+      </h2>
+    </main>
+  );
+};
+
+export default History;
