c65315be845bfd7d1b0ae3257fb75e5c8ffda7a9
VicenteSina98 <133327651+VicenteSina98@users.noreply.github.com>
2024-03-07 08:24:54 -0300

    💄 mejoras UI en mensajes y opciones del chat

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -2,13 +2,9 @@ import PropTypes from "prop-types";
 import { Paragraph } from "../UI/base";
 
 const Message = ({ data, isIA }) => {
-  const textAlign = "text-left";
-  const padding = "px-4 py-2";
-  const width = "max-w-max";
   const borderRadius = "rounded-md";
   const bgIa = "bg-neutral-200 dark:bg-neutral-700";
   const bgUser = "bg-neutral-100 dark:bg-neutral-600";
-  const shadow = "shadow-md dark:shadow-lg";
   const baseClasses = ["w-full", "flex", "gap-2", "break-words"].join(" ");
   const isIaClasses = [baseClasses, "flex-col"].join(" ");
   const isUserClasses = [baseClasses, "justify-end"].join(" ");
@@ -18,23 +14,25 @@ const Message = ({ data, isIA }) => {
       <div className={isPredictionClasses}>
         <Paragraph
           content={data?.split("\n").filter((d) => d !== "")}
-          textAlign={textAlign}
-          padding={padding}
-          width={width}
+          textAlign="text-left"
+          textSize="text-sm"
+          padding="px-4 py-2"
+          //width="max-w-max"
           backgroundColor={bgIa}
-          shadow={shadow}
+          shadow="shadow-md dark:shadow-lg"
         />
       </div>
     ) : (
       <div className={isIaClasses}>
         <Paragraph
           content={data}
-          textAlign={textAlign}
-          padding={padding}
-          width={width}
+          textAlign="text-left"
+          textSize="text-sm"
+          padding="px-4 py-2"
+          width="max-w-max"
           backgroundColor={bgIa}
           borderRadius={borderRadius}
-          shadow={shadow}
+          shadow="shadow-md dark:shadow-lg"
         />
       </div>
     )
@@ -42,12 +40,13 @@ const Message = ({ data, isIA }) => {
     <div className={isUserClasses}>
       <Paragraph
         content={data}
-        textAlign={textAlign}
-        padding={padding}
-        width={width}
+        textAlign="text-left"
+        textSize="text-sm"
+        padding="px-4 py-2"
+        width="max-w-max"
         backgroundColor={bgUser}
         borderRadius={borderRadius}
-        shadow={shadow}
+        shadow="shadow-md dark:shadow-lg"
         hidden={!data ? "hidden" : ""}
       />
     </div>
